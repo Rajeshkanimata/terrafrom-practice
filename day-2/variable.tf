4cc38c52094c941a852e43ea3d20b6353a89be8c
rajeshkanimata <rajeshkanimata123@gmail.com>
2025-10-28 18:36:45 +0530

    variable.tf

---
+++ day-2/variable.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+    description = "ami calling"
+    default = "ami-00ecbb8ba07f65d41"
+    type = string
+}
+
+variable "type" {
+    description = "type calling"
+    default = "t3.micro"
+    type = string
+  
+}
