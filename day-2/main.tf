7b084e1b94e9a10e0f595b70a51fcba91d490949
rajeshkanimata <rajeshkanimata123@gmail.com>
2025-10-28 18:51:45 +0530

    day2end

--- day-2/main.tf
+++ day-2/main.tf
@@ -1,14 +1,15 @@
-resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "name" {
-    cidr_block = "10.0.0.0/24"
-    vpc_id = aws_vpc.name.id
+resource "aws_subnet" "my_subnet" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id     = aws_vpc.my_vpc.id
 }
 
-resource "aws_instance" "name" {
-    ami = "ami-00ecbb8ba07f65d41"
-    instance_type = "t3.micro"
-    subnet_id = aws_subnet.name.id
+resource "aws_instance" "my_instance" {
+  ami           = "ami-00ecbb8ba07f65d41"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.my_subnet.id
 }
+
