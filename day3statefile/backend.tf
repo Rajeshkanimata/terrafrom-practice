d29d3d3fc4bfd1e0e49385bffa7550eaa2502959
rajeshkanimata <rajeshkanimata123@gmail.com>
2025-10-30 12:22:21 +0530

    4

--- day3statefile/backend.tf
+++ day3statefile/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-file-storage-test-rajesh "
+    bucket = "terraform-state-file-storage-test-rajesh"
     key = "terraform.tfstate"
     region = "us-east-1"
   }
