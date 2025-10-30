66bbc841b5d396141e94201896478c0ba5b27af7
rajeshkanimata <rajeshkanimata123@gmail.com>
2025-10-30 16:34:18 +0530

    backend-updated

--- day-4-statelocking/backend.tf
+++ day-4-statelocking/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-file-storage-test-rajesh"
     key = "terraform.tfstate"
-    use_lockfile = true
+    #use_lockfile = true #state lock to avoid conflict
+    dynamodb_table = "rajesh"
+    encrypt = true
     region = "us-east-1"
   }
 }
